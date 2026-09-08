ca8dd4a13b2d08826b19662ebd0587b20806edce
Timo <dte0011@edu.curio.nl>
2026-09-08 09:48:00 +0200

    create+h2
    index gemaakt en stukje body gemaakt h2, met placeholde lorem.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Software Developer</title>
+</head>
+<body>
+  <h2>curio</h2>
+  <p>MBO VMBO CONTACT</p>
+
+  <h1>Software Developer</h1>
+  <p>Home Open dag Software Developer</p>
+  <p>GEMAAKT DOOR: Timo van Eck</p>
+  <p>JAAR: 2</p>
+  <p>NIVEAU: 4</p>
+  <p>Mijn ervaring</p>
+  <p>Open dagen</p>
+
+  <h2>In het kort</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aperiam ducimus consectetur optio dolore, dolorum sint magni minima temporibus sit voluptates officiis unde ex accusantium odio natus nam ea maiores!</p>
+
+  <h2>Sfeer op de afdeling</h2>
+  <h2>Wat vind ik van de opleiding</h2>
+  <h2>Rooster eerstejaars</h2>
+  <h2>Groepswerk</h2>
+  <h2>Huiswerk</h2>
+
+  <p>Meer weten over de opleiding? Bezoek curio.nl</p>
+  <p>Kom naar de open dag. Bekijk data</p>
+
+  <footer>
+    <?php echo date("Y"); ?> Curio Software Developer
+  </footer>
+</body>
+</html>
